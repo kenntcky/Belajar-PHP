9bb3c840ff91d43a65cfa081026a6c3821c24d06
kenntcky <hologrampulsion@gmail.com>
2024-02-23 22:50:22 +0700

    Session login fix

--- PHPWeb/session-login/login.php
+++ PHPWeb/session-login/login.php
@@ -1,11 +1,12 @@
 <?php
 
 session_start();
-$_SESSION['login'] = false;
 
-if ($_SESSION['login']) {
-    header('Location: /BelajarPHP/PHPWeb/session-login/index.php');
-    exit();
+if (isset($_SESSION['login'])) {
+    if ($_SESSION['login']) {
+        header('Location: /BelajarPHP/PHPWeb/session-login/index.php');
+        exit();
+    }
 }
 
 $error = "";
